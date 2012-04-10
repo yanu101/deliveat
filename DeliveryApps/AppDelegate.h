cf27b3e7e668f7ee224440ec632dfde936ecea2b
Yanuar Rahman <yanuar@objectleft-lm.jakarta.corp.yahoo.com>
2012-04-10 14:32:56 +0700

    Initial Commit

---
+++ DeliveryApps/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DeliveryApps
+//
+//  Created by Yanuar Rahman on 4/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DeliveryApps/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DeliveryApps
+//
+//  Created by Yanuar Rahman on 4/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
